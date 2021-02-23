bc8e6a16af588eb7fc732be82d029b131881ef23
Deyan Todorov <70479102+DeyanTodorov17@users.noreply.github.com>
2021-02-23 15:19:23 +0200

    Add a register functionality

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,35 +1,50 @@
+import { Router } from "@vaadin/router";
+
 const authService = {
-    getForm(target,...x){
-        let formData = new FormData(target)
-        return x.reduce((acc,param)=>{
-            acc[param] = formData.get(param);
-            return acc;
-        },{})
-    },
-    getData(){
-        if(localStorage.getItem("user")){
-            let email = JSON.parse(localStorage.getItem("user")).email
-            return {
-                isAuthenticated: true,
-                email
-            }
-        }
-        return {isAuthenticated:false}
-    },
-    register(e){
-        e.preventDefault()
-        const {email,password,repeatPassword} = this.getForm(e.target.parentElement,"email","password","repeatPassword")
-        if(password !== repeatPassword){
-            return
-        }
-        console.log(firebase.auth());
-    },
-    login(){
-
-    },
-    logout(){
-
+  getForm(target, ...x) {
+    let formData = new FormData(target);
+    return x.reduce((acc, param) => {
+      acc[param] = formData.get(param);
+      return acc;
+    }, {});
+  },
+  getData() {
+    if (localStorage.getItem("user")) {
+      const email = JSON.parse(localStorage.getItem("user")).email
+      return {
+        isAuthenticated: true,
+        email,
+      };
     }
-}
+    return { isAuthenticated: false };
+  },
+  register(e) {
+    e.preventDefault();
+    const { email, password, repeatPassword } = this.getForm(
+      e.target.parentElement,
+      "email",
+      "password",
+      "repeatPassword"
+    );
+    if (password !== repeatPassword) {
+      return;
+    }
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
+      .then((userCredential) => {
+        localStorage.setItem("user",JSON.stringify({
+            email: userCredential.user.email
+        }))
+        Router.go("/")
+      })
+      .catch((error) => {
+        alert(error.message)
+        Router.go("/")
+      });
+  },
+  login() {},
+  logout() {},
+};
 
-export default authService
+export default authService;
